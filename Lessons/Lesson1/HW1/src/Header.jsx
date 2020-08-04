2bd2ece61cc321b555d6498568c5e7d38b1e29cd
SonOfLion <syplusplus@gmail.com>
2020-08-04 18:48:49 +0300

    1

--- Lessons/Lesson1/HW1/src/Header.jsx
+++ Lessons/Lesson1/HW1/src/Header.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import UserMenu from './UserMenu';
 // import { Context } from './Context';
 
-function Header() {
-    return(
-        <header className="header">
-            <UserMenu/>
-        </header>
-    );
+class Header extends Component {
+    render() {
+        return(
+            <header className="header">
+                <UserMenu/>
+            </header>
+        );
+    };
 };
 
 export default Header;
