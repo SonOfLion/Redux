d12394c7fdf7e8a221d816bb89972b37d3d25ea9
SonOfLion <syplusplus@gmail.com>
2020-08-06 17:52:08 +0300

    2

--- Lessons/Lesson2/Task2/src/store.js
+++ Lessons/Lesson2/Task2/src/store.js
@@ -33,14 +33,14 @@ const counterReducer = (state = initialState, action) => {
         return {
             ...state,
             value: state.value + 1,
-            history: state.history.concat(+1)
+            history: state.history.concat('+1')
         };
 
         case DECREMENT:
         return {
             ...state,
             value: state.value - 1,
-            history: state.history.concat(-1)
+            history: state.history.concat('-1')
         };
 
         case RESET:
