05464ca5a936ed6496d3a506cba42017ddd9dd31
SonOfLion <syplusplus@gmail.com>
2020-08-06 17:46:16 +0300

    2

--- Lessons/Lesson2/Task2/src/index.js
+++ Lessons/Lesson2/Task2/src/index.js
@@ -25,10 +25,9 @@ resetBtn.addEventListener('click', onReset)
 
 store.subscribe(() => {
     const state = store.getState();
-    const currentValue = state.history.reduce((acc, value) => acc + Number(value), 0);
+    const currentValue = state.history.reduce((acc, value) =>
+        acc + Number(value), 0);
     const historyString = state.history.join('');
     resultElem.textContent = state.history.length === 0
-    ? ''
-    : `${historyString} = ${currentValue}`;
-        
+    ? '' : `${historyString} = ${currentValue}`;
 });
